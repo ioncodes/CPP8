f2fe9e44bd61b33c5307bcdacd39d028ae2faf74
Luca Marcelli <ionix@protonmail.com>
2017-05-25 19:48:38 +0200

    apply style

--- Chip8/Chip8.cpp
+++ Chip8/Chip8.cpp
@@ -13,15 +13,15 @@ Chip8::Chip8()
 
 void Chip8::Reset()
 {
-	programCounter = 0x200;
-	opcode = 0x000;
-	indexRegister = 0x000;
-	stackPointer = 0x000;
-	soundTimer = 0x000;
-	delayTimer = 0x000;
-	memory = {0x000};
-	stack = {0x000};
-	virtualRegisters = {0x000};
+	m_programCounter = 0x200;
+	m_opcode = 0x000;
+	m_indexRegister = 0x000;
+	m_stackPointer = 0x000;
+	m_soundTimer = 0x000;
+	m_delayTimer = 0x000;
+	m_memory = {0x000};
+	m_stack = {0x000};
+	m_virtualRegisters = {0x000};
 	m_screen = {0x000};
 }
 
@@ -36,7 +36,7 @@ void Chip8::LoadFontSet()
 	// Load font into memory
 	for (int i = 0; i < FONT_SIZE; i++)
 	{
-		memory.at(i) = static_cast<int>(FONT_SET.at(i));
+		m_memory.at(i) = static_cast<int>(FONT_SET.at(i));
 	}
 }
 
@@ -61,226 +61,226 @@ void Chip8::LoadRom(const char* file_path)
 
 	for (int i = 0; i < buffer.size(); i++)
 	{
-		memory.at(programCounter + i) = static_cast<unsigned int>(buffer.at(i));
+		m_memory.at(m_programCounter + i) = static_cast<unsigned int>(buffer.at(i));
 	}
 }
 
 void Chip8::Cycle()
 {
-	opcode = memory.at(programCounter) << 8 | memory.at(programCounter + 1);
+	m_opcode = m_memory.at(m_programCounter) << 8 | m_memory.at(m_programCounter + 1);
 
-	if (opcode & 0xF000 == 0x0000)
+	if (m_opcode & 0xF000 == 0x0000)
 	{
-		switch (instructions.getInstruction(opcode & 0x000F))
+		switch (m_instructions.getInstruction(m_opcode & 0x000F))
 		{
 		case Instructions::Clear:
 			m_screen = {0};
-			programCounter += 2;
+			m_programCounter += 2;
 			break;
 
 		case Instructions::ReturnToSubroutine:
-			--stackPointer;
-			programCounter = stack[stackPointer];
-			programCounter += 2;
+			--m_stackPointer;
+			m_programCounter = m_stack[m_stackPointer];
+			m_programCounter += 2;
 			break;
 		case Instructions::InstructionUnknown:
-			std::cout << "Opcode not found: " << std::hex << (opcode & 0x000F) << std::endl;
+			std::cout << "Opcode not found: " << std::hex << (m_opcode & 0x000F) << std::endl;
 		default: 
 			std::cout << "Unknown Error" << std::endl;
 		}
 	}
 	else
 	{
-		switch (instructions.getInstruction(opcode & 0xF000))
+		switch (m_instructions.getInstruction(m_opcode & 0xF000))
 		{
 		case Instructions::JumpToAddress:
-			programCounter = opcode & 0x0FFF;
+			m_programCounter = m_opcode & 0x0FFF;
 			break;
 
 		case Instructions::CallSubroutine:
-			stack[stackPointer] = programCounter;
-			++stackPointer;
-			programCounter = opcode & 0x0FFF;
+			m_stack[m_stackPointer] = m_programCounter;
+			++m_stackPointer;
+			m_programCounter = m_opcode & 0x0FFF;
 			break;
 
 		case Instructions::SkipInstructionIfVirtualEqualsNN:
-			if (virtualRegisters[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
-				programCounter += 4;
+			if (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] == (m_opcode & 0x00FF))
+				m_programCounter += 4;
 			else
-				programCounter += 2;
+				m_programCounter += 2;
 			break;
 
 		case Instructions::SkipInstructionIfVirtualEqualsNotNN:
-			if (virtualRegisters[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
-				programCounter += 4;
+			if (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] != (m_opcode & 0x00FF))
+				m_programCounter += 4;
 			else
-				programCounter += 2;
+				m_programCounter += 2;
 			break;
 
 		case Instructions::SkipInstructionIfVirtualEqualsVirtual:
-			if (virtualRegisters[(opcode & 0x0F00) >> 8] == virtualRegisters[(opcode & 0x00F0) >> 4])
-				programCounter += 4;
+			if (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] == m_virtualRegisters[(m_opcode & 0x00F0) >> 4])
+				m_programCounter += 4;
 			else
-				programCounter += 2;
+				m_programCounter += 2;
 			break;
 
 		case Instructions::SetVirtualToNN:
-			virtualRegisters[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
-			programCounter += 2;
+			m_virtualRegisters[(m_opcode & 0x0F00) >> 8] = m_opcode & 0x00FF;
+			m_programCounter += 2;
 			break;
 
 		case Instructions::AddNNToVirtual:
-			virtualRegisters[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
-			programCounter += 2;
+			m_virtualRegisters[(m_opcode & 0x0F00) >> 8] += m_opcode & 0x00FF;
+			m_programCounter += 2;
 			break;
 
 		case Instructions::Virtuals:
-			switch (instructions.getVirtual(opcode & 0x000F))
+			switch (m_instructions.getVirtual(m_opcode & 0x000F))
 			{
 			case Instructions::SetVirtualToVirtual:
-				virtualRegisters[(opcode & 0x0F00) >> 8] = virtualRegisters[(opcode & 0x00F0) >> 4];
-				programCounter += 2;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] = m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SetVirtualToVirtualOR:
-				virtualRegisters[(opcode & 0x0F00) >> 8] |= virtualRegisters[(opcode & 0x00F0) >> 4];
-				programCounter += 2;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] |= m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SetVirtualToVirtualAND:
-				virtualRegisters[(opcode & 0x0F00) >> 8] &= virtualRegisters[(opcode & 0x00F0) >> 4];
-				programCounter += 2;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] &= m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SetVirtualToVirtualXOR:
-				virtualRegisters[(opcode & 0x0F00) >> 8] ^= virtualRegisters[(opcode & 0x00F0) >> 4];
-				programCounter += 2;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] ^= m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::AddVirtualToVirtual:
-				virtualRegisters[(opcode & 0x0F00) >> 8] += virtualRegisters[(opcode & 0x00F0) >> 4];
-				if (virtualRegisters[(opcode & 0x00F0) >> 4] > (0xFF - virtualRegisters[(opcode & 0x0F00) >> 8]))
-					virtualRegisters[0xF] = 1;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] += m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				if (m_virtualRegisters[(m_opcode & 0x00F0) >> 4] > (0xFF - m_virtualRegisters[(m_opcode & 0x0F00) >> 8]))
+					m_virtualRegisters[0xF] = 1;
 				else
-					virtualRegisters[0xF] = 0;
-				programCounter += 2;
+					m_virtualRegisters[0xF] = 0;
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SubVirtualFromVirtual:
-				if (virtualRegisters[(opcode & 0x00F0) >> 4] > virtualRegisters[(opcode & 0x0F00) >> 8])
-					virtualRegisters[0xF] = 0;
+				if (m_virtualRegisters[(m_opcode & 0x00F0) >> 4] > m_virtualRegisters[(m_opcode & 0x0F00) >> 8])
+					m_virtualRegisters[0xF] = 0;
 				else
-					virtualRegisters[0xF] = 1;
-				virtualRegisters[(opcode & 0x0F00) >> 8] -= virtualRegisters[(opcode & 0x00F0) >> 4];
-				programCounter += 2;
+					m_virtualRegisters[0xF] = 1;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] -= m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::ShiftVirtualRight:
-				virtualRegisters[0xF] = virtualRegisters[(opcode & 0x0F00) >> 8] & 0x1;
-				virtualRegisters[(opcode & 0x0F00) >> 8] >>= 1;
-				programCounter += 2;
+				m_virtualRegisters[0xF] = m_virtualRegisters[(m_opcode & 0x0F00) >> 8] & 0x1;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] >>= 1;
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SetVirtualToVirtualMinus:
-				if (virtualRegisters[(opcode & 0x0F00) >> 8] > virtualRegisters[(opcode & 0x00F0) >> 4])
-					virtualRegisters[0xF] = 0;
+				if (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] > m_virtualRegisters[(m_opcode & 0x00F0) >> 4])
+					m_virtualRegisters[0xF] = 0;
 				else
-					virtualRegisters[0xF] = 1;
-				virtualRegisters[(opcode & 0x0F00) >> 8] = virtualRegisters[(opcode & 0x00F0) >> 4] - virtualRegisters[(opcode & 0x0F00) >> 8];
-				programCounter += 2;
+					m_virtualRegisters[0xF] = 1;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] = m_virtualRegisters[(m_opcode & 0x00F0) >> 4] - m_virtualRegisters[(m_opcode & 0x0F00) >> 8];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::ShiftVirtualLeft:
-				virtualRegisters[0xF] = virtualRegisters[(opcode & 0x0F00) >> 8] >> 7;
-				virtualRegisters[(opcode & 0x0F00) >> 8] <<= 1;
-				programCounter += 2;
+				m_virtualRegisters[0xF] = m_virtualRegisters[(m_opcode & 0x0F00) >> 8] >> 7;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] <<= 1;
+				m_programCounter += 2;
 				break;
 			case Instructions::VirtualUnknown:
-				std::cout << "Opcode not found: " << std::hex << (opcode & 0x000F) << std::endl;
+				std::cout << "Opcode not found: " << std::hex << (m_opcode & 0x000F) << std::endl;
 			default:
 				std::cout << "Unknown Error" << std::endl;;
 			}
 			break;
 
 		case Instructions::SkipInstructionIfVirtualEqualsNotVirtual:
-			if (virtualRegisters[(opcode & 0x0F00) >> 8] != virtualRegisters[(opcode & 0x00F0) >> 4])
-				programCounter += 4;
+			if (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] != m_virtualRegisters[(m_opcode & 0x00F0) >> 4])
+				m_programCounter += 4;
 			else
-				programCounter += 2;
+				m_programCounter += 2;
 			break;
 
 		case Instructions::SetIndexRegisterToNNN:
-			indexRegister = opcode & 0x0FFF;
-			programCounter += 2;
+			m_indexRegister = m_opcode & 0x0FFF;
+			m_programCounter += 2;
 			break;
 
 		case Instructions::JumpToAddressPlusV0:
-			programCounter = (opcode & 0x0FFF) + virtualRegisters[0];
+			m_programCounter = (m_opcode & 0x0FFF) + m_virtualRegisters[0];
 			break;
 
 		case Instructions::SetVirtualToRandom:
-			virtualRegisters[(opcode & 0x0F00) >> 8] = (rand() % (0xFF + 1)) & (opcode & 0x00FF);
-			programCounter += 2;
+			m_virtualRegisters[(m_opcode & 0x0F00) >> 8] = (rand() % (0xFF + 1)) & (m_opcode & 0x00FF);
+			m_programCounter += 2;
 			break;
 
 		case Instructions::DrawSprite:
 			{
-				unsigned short x = virtualRegisters[(opcode & 0x0F00) >> 8];
-				unsigned short y = virtualRegisters[(opcode & 0x00F0) >> 4];
-				unsigned short height = opcode & 0x000F;
+				unsigned short x = m_virtualRegisters[(m_opcode & 0x0F00) >> 8];
+				unsigned short y = m_virtualRegisters[(m_opcode & 0x00F0) >> 4];
+				unsigned short height = m_opcode & 0x000F;
 
-				virtualRegisters[0xF] = 0;
+				m_virtualRegisters[0xF] = 0;
 				for (int yline = 0; yline < height; yline++)
 				{
-					unsigned short pixel = memory[indexRegister + yline];
+					unsigned short pixel = m_memory[m_indexRegister + yline];
 					for (int xline = 0; xline < 8; xline++)
 					{
 						if ((pixel & (0x80 >> xline)) != 0)
 						{
 							if (m_screen[(x + xline + ((y + yline) * 64))] == 1)
 							{
-								virtualRegisters[0xF] = 1;
+								m_virtualRegisters[0xF] = 1;
 							}
 							m_screen[x + xline + ((y + yline) * 64)] ^= 1;
 						}
 					}
 				}
 
-				programCounter += 2;
+				m_programCounter += 2;
 			}
 			break;
 
 		case Instructions::Keys:
 
-			switch (instructions.getKey(opcode & 0x00FF))
+			switch (m_instructions.getKey(m_opcode & 0x00FF))
 			{
 			case Instructions::SkipIfPressed:
-				if (keypad[virtualRegisters[(opcode & 0x0F00) >> 8]] != 0)
-					programCounter += 4;
+				if (m_keypad[m_virtualRegisters[(m_opcode & 0x0F00) >> 8]] != 0)
+					m_programCounter += 4;
 				else
-					programCounter += 2;
+					m_programCounter += 2;
 				break;
 
 			case Instructions::SkipIfNotPressed:
-				if (keypad[virtualRegisters[(opcode & 0x0F00) >> 8]] == 0)
-					programCounter += 4;
+				if (m_keypad[m_virtualRegisters[(m_opcode & 0x0F00) >> 8]] == 0)
+					m_programCounter += 4;
 				else
-					programCounter += 2;
+					m_programCounter += 2;
 				break;
 
 			case Instructions::KeyUnknown:
-				std::cout << "Opcode not found: " << std::hex << (opcode & 0x00FF) << std::endl;
+				std::cout << "Opcode not found: " << std::hex << (m_opcode & 0x00FF) << std::endl;
 			default:
 				std::cout << "Unknown Error" << std::endl;
 			}
 			break;
 
 		case Instructions::FXS:
-			switch (instructions.getFX(opcode & 0x00FF))
+			switch (m_instructions.getFX(m_opcode & 0x00FF))
 			{
 			case Instructions::SetVirtualToDelayTimer:
-				virtualRegisters[(opcode & 0x0F00) >> 8] = delayTimer;
-				programCounter += 2;
+				m_virtualRegisters[(m_opcode & 0x0F00) >> 8] = m_delayTimer;
+				m_programCounter += 2;
 				break;
 
 			case Instructions::WaitForKeyAndStore:
@@ -289,9 +289,9 @@ void Chip8::Cycle()
 
 					for (int i = 0; i < 16; ++i)
 					{
-						if (keypad[i] != 0)
+						if (m_keypad[i] != 0)
 						{
-							virtualRegisters[(opcode & 0x0F00) >> 8] = i;
+							m_virtualRegisters[(m_opcode & 0x0F00) >> 8] = i;
 							key_pressed = true;
 						}
 					}
@@ -299,64 +299,64 @@ void Chip8::Cycle()
 					if (!key_pressed)
 						return;
 
-					programCounter += 2;
+					m_programCounter += 2;
 				}
 				break;
 
 			case Instructions::SetDelayTimerToVirtual:
-				delayTimer = virtualRegisters[(opcode & 0x0F00) >> 8];
-				programCounter += 2;
+				m_delayTimer = m_virtualRegisters[(m_opcode & 0x0F00) >> 8];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SetSoundTimerToVirtual:
-				soundTimer = virtualRegisters[(opcode & 0x0F00) >> 8];
-				programCounter += 2;
+				m_soundTimer = m_virtualRegisters[(m_opcode & 0x0F00) >> 8];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::AddVirtualToIndex:
-				if (indexRegister + virtualRegisters[(opcode & 0x0F00) >> 8] > 0xFFF)
-					virtualRegisters[0xF] = 1;
+				if (m_indexRegister + m_virtualRegisters[(m_opcode & 0x0F00) >> 8] > 0xFFF)
+					m_virtualRegisters[0xF] = 1;
 				else
-					virtualRegisters[0xF] = 0;
-				indexRegister += virtualRegisters[(opcode & 0x0F00) >> 8];
-				programCounter += 2;
+					m_virtualRegisters[0xF] = 0;
+				m_indexRegister += m_virtualRegisters[(m_opcode & 0x0F00) >> 8];
+				m_programCounter += 2;
 				break;
 
 			case Instructions::SetIndexRegisterToVirtualSprite:
-				indexRegister = virtualRegisters[(opcode & 0x0F00) >> 8] * 0x5;
-				programCounter += 2;
+				m_indexRegister = m_virtualRegisters[(m_opcode & 0x0F00) >> 8] * 0x5;
+				m_programCounter += 2;
 				break;
 
 			case Instructions::Decimals:
-				memory[indexRegister] = virtualRegisters[(opcode & 0x0F00) >> 8] / 100;
-				memory[indexRegister + 1] = (virtualRegisters[(opcode & 0x0F00) >> 8] / 10) % 10;
-				memory[indexRegister + 2] = (virtualRegisters[(opcode & 0x0F00) >> 8] % 100) % 10;
-				programCounter += 2;
+				m_memory[m_indexRegister] = m_virtualRegisters[(m_opcode & 0x0F00) >> 8] / 100;
+				m_memory[m_indexRegister + 1] = (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] / 10) % 10;
+				m_memory[m_indexRegister + 2] = (m_virtualRegisters[(m_opcode & 0x0F00) >> 8] % 100) % 10;
+				m_programCounter += 2;
 				break;
 
 			case Instructions::StoreVirtualToMemory1:
-				for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
-					memory[indexRegister + i] = virtualRegisters[i];
+				for (int i = 0; i <= ((m_opcode & 0x0F00) >> 8); ++i)
+					m_memory[m_indexRegister + i] = m_virtualRegisters[i];
 
-				indexRegister += ((opcode & 0x0F00) >> 8) + 1;
-				programCounter += 2;
+				m_indexRegister += ((m_opcode & 0x0F00) >> 8) + 1;
+				m_programCounter += 2;
 				break;
 
 			case Instructions::StoreVirtualToMemory2:
-				for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
-					virtualRegisters[i] = memory[indexRegister + i];
+				for (int i = 0; i <= ((m_opcode & 0x0F00) >> 8); ++i)
+					m_virtualRegisters[i] = m_memory[m_indexRegister + i];
 
-				indexRegister += ((opcode & 0x0F00) >> 8) + 1;
-				programCounter += 2;
+				m_indexRegister += ((m_opcode & 0x0F00) >> 8) + 1;
+				m_programCounter += 2;
 				break;
 			case Instructions::UnknownFX:
-				std::cout << "Opcode not found: " << std::hex << (opcode & 0x00FF) << std::endl;
+				std::cout << "Opcode not found: " << std::hex << (m_opcode & 0x00FF) << std::endl;
 			default:
 				std::cout << "Unknown Error" << std::endl;;
 			}
 			break;
 		case Instructions::InstructionUnknown:
-			std::cout << "Opcode not found: " << std::hex << (opcode & 0xF000) << std::endl;
+			std::cout << "Opcode not found: " << std::hex << (m_opcode & 0xF000) << std::endl;
 		default:
 			std::cout << "Unknown Error" << std::endl;; ;
 		}
--- Chip8/Chip8.h
+++ Chip8/Chip8.h
@@ -4,23 +4,22 @@
 
 class Chip8
 {
-private:
 	static const unsigned int MEMORY_SIZE					= 4096;			// Memory Size
 	static const unsigned int GLOBAL_SIZE					= 0x10;			// General size is 16bit
 	static const unsigned int FONT_SIZE						= 0x50;			// Font Size = 80
 	static const unsigned int SCREEN_SIZE					= 0x800;		// Screen Size 2048; 32 * 64
-	unsigned int programCounter								= 0x200;		// Program Counter
-	unsigned int opcode										= 0x000;		// Current Opcode
-	unsigned int stackPointer								= 0x000;		// Stack Pointer
-	std::array<unsigned int, MEMORY_SIZE> memory			= { 0x000 };	// Memory
-	std::array<unsigned int, GLOBAL_SIZE> virtualRegisters	= { 0x000 };	// Virtual Registers -> V[x]; 16 registers
-	unsigned int indexRegister								= 0x000;		// I [Index Register]
-	unsigned int soundTimer									= 0x000;		// Timer for the sound
-	unsigned int delayTimer									= 0x000;		// Timer for the delay
-	std::array<unsigned int, GLOBAL_SIZE> keypad			= { 0x000 };	// The keypad
-	std::array<unsigned int, GLOBAL_SIZE> stack				= { 0x000 };	// 16bit Stack
+	unsigned int m_programCounter								= 0x200;		// Program Counter
+	unsigned int m_opcode										= 0x000;		// Current Opcode
+	unsigned int m_stackPointer								= 0x000;		// Stack Pointer
+	std::array<unsigned int, MEMORY_SIZE> m_memory			= { 0x000 };	// Memory
+	std::array<unsigned int, GLOBAL_SIZE> m_virtualRegisters	= { 0x000 };	// Virtual Registers -> V[x]; 16 registers
+	unsigned int m_indexRegister								= 0x000;		// I [Index Register]
+	unsigned int m_soundTimer									= 0x000;		// Timer for the sound
+	unsigned int m_delayTimer									= 0x000;		// Timer for the delay
+	std::array<unsigned int, GLOBAL_SIZE> m_keypad			= { 0x000 };	// The keypad
+	std::array<unsigned int, GLOBAL_SIZE> m_stack				= { 0x000 };	// 16bit Stack
 	std::array<unsigned int, SCREEN_SIZE> m_screen			= { 0x000 };	// The screen
-	Instructions instructions;												// Instructions
+	Instructions m_instructions;												// Instructions
 	const std::array<unsigned char, FONT_SIZE> FONT_SET =					// The fontset
 	{
 		0xF0, 0x90, 0x90, 0x90, 0xF0, //0
